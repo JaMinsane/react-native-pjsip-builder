1f6b1a6a1ec39c786288b464f5c04af55a4706b3
James JH <46429005+JaMinsane@users.noreply.github.com>
2021-11-04 04:44:46 -0500

    Update build_pjsip.sh

--- android/build_pjsip.sh
+++ android/build_pjsip.sh
@@ -8,14 +8,15 @@ cp -r /sources/pjsip /tmp/pjsip
 # TODO: Use flags like in vialerpjsip for config.h
 cat <<EOF > "/tmp/pjsip/pjlib/include/pj/config_site.h"
 #define PJ_CONFIG_ANDROID 1
+#include <pj/config_site_sample.h>
+EOF
+
 #define PJMEDIA_HAS_G729_CODEC 1
 #define PJMEDIA_HAS_G7221_CODEC 1
-#include <pj/config_site_sample.h>
-#define PJMEDIA_HAS_VIDEO 0
+#define PJMEDIA_HAS_VIDEO 1
 #define PJMEDIA_AUDIO_DEV_HAS_ANDROID_JNI 0
 #define PJMEDIA_AUDIO_DEV_HAS_OPENSL 1
 #define PJSIP_AUTH_AUTO_SEND_NEXT 0
-EOF
 
 cd /tmp/pjsip
 
